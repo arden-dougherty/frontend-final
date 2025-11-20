196f94a1420f25822c5f14e69d490d058b12d5b2
Arden Dougherty <asd6@pdx.edu>
2025-11-20 00:05:50 -0800

    axe setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { StrictMode } from "react";
+import { ReactDOM, createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+if (import.meta.env.DEV) {
+  import("@axe-core/react").then(({ default: axe }) => {
+    axe(React, ReactDOM, 1000);
+  });
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
